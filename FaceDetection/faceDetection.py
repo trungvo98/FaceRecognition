913175b735dbd0b46c1280de12f29ce12687dcb9
trungvo <sportking1998@gmail.com>
2020-04-23 15:24:19 +0700

    Init

---
+++ FaceDetection/faceDetection.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+import cv2
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor= 1.3 , #how much the image size is reduced at each image scale.
+        minNeighbors = 5 , #how many neighbors each candidate rectangle should have to retain it.
+        minSize =(30,30) #Minimum possible object size
+        )
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ FaceDetection/faceSmileDetection.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+import cv2
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor= 1.3, #how much the image size is reduced at each image scale.
+        minNeighbors =5 , #how many neighbors each candidate rectangle should have to retain it.
+        minSize =(30,30) #Minimum possible object size
+        )
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        smile = smile_cascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=10,
+            minSize=(25, 25),
+            )
+        for (sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,255,0),2)
+
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
